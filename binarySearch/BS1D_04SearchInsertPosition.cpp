57a0cf72b8008acd1b4cfee8051c52f397ac7fd8
Krishnakant chourasiya <krishnkantchaurasia8889@gmail.com>
2026-05-24 19:32:15 +0530

    Add files via upload

---
+++ binarySearch/BS1D_04SearchInsertPosition.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int LowerBound(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    int ans = n;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (nums[mid] >= target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid+1;
+        }
+    }
+
+    return ans;
+}
+int main()
+{
+    vector<int> arr = {-1, 0, 3, 5, 9, 12};
+    int target = 9;
+    int ans = LowerBound(arr, 9);
+    cout << ans;
+    return 0;
+}
